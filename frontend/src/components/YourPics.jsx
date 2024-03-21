a08c9b8b54b06e3745b772089e11e4926813d070
Jagss24 <btwitsjagannath12@gmail.com>
2024-03-21 11:55:43 +0530

    User can see their pics which they have saved in their pics and can delete also if they don't want it.

--- frontend/src/components/YourPics.jsx
+++ frontend/src/components/YourPics.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import basePort from '../basePort'
+import axios from 'axios'
+import { MdDelete } from 'react-icons/md';
 
 const YourPics = () => {
+  const [yourPics, setYourPics] = useState([])
+  const port = basePort
+  const user = JSON.parse(sessionStorage.getItem('userData'))
+  if (!user) {
+    alert('Please Signin First')
+    window.location.href = '/signin'
+  }
+  const getPics = async () => {
+    await axios.get(`${port}/api/v2/yourPics/${user.emailId}`).then((res => {
+      setYourPics(res.data.yourPics)
+    }))
+  }
+  useEffect(() => {
+    getPics()
+  }, [user])
+
+
+  const handleDelete = async (pic_url) => {
+    const normalizedPicUrl = pic_url.slice(8)
+    console.log(normalizedPicUrl)
+    console.log(user.emailId)
+    const encodedUrl = `http://localhost:1000/api/v2/deletePic/${user.emailId}/uploads\\${normalizedPicUrl}`.replace(/\\/g, '%5C');
+    await axios.delete(encodedUrl).then((res) => {
+      console.log(res.data)
+      getPics()
+    })
+  }
+
   return (
-    <div>YourPics</div>
+    <div className='flex flex-wrap gap-4' >
+      {yourPics.map((pic, index) => (
+        <div key={index} >
+          <img src={`${port}/${pic}`} alt='images' style={{ width: '200px' }} className='border rounded-lg' />
+          <MdDelete onClick={() => handleDelete(pic)} className='rounde-full shadow-md text-red-500 cursor-pointer' fontSize={25} />
+        </div>
+      ))}
+
+
+
+    </div>
   )
 }
 
