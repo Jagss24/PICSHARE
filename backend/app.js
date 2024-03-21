9cd28f22c78fe86820151eb727c9a1acd0670b41
Jagss24 <btwitsjagannath12@gmail.com>
2024-03-21 11:41:44 +0530

    Added the image delete functionality, Removed the ShareWithOthers modal. Added navigation to login page

--- backend/app.js
+++ backend/app.js
@@ -6,13 +6,17 @@ conn(process.env.MONGO_URL);
 import auth from "./routes/auth.js";
 import images from "./routes/image.js";
 import cors from "cors";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = 1000;
 
 app.use(express.json());
 app.use(cors());
-
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/v1", auth);
 app.use("/api/v2", images);
 
