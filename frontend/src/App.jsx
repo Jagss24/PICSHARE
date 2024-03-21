e0bedc9452e83f365bd23703d4848a7727261adc
Jagss24 <btwitsjagannath12@gmail.com>
2024-03-21 11:45:29 +0530

    Removed the sharedwithother modal and imported useffect hook

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import CustomWebcam from './components/CustomWebcam';
@@ -7,10 +7,10 @@ import SignIn from './components/SignIn';
 import YourPics from './components/YourPics';
 import SignUp from './components/SignUp';
 import UploadPics from './components/UploadPics';
-import ShareWithOthers from './components/ShareWithOthers';
 import Modal from './components/Modal';
-
 const App = () => {
+
+
   return (
     <Router>
       <div>
@@ -22,7 +22,6 @@ const App = () => {
           <Route path='/camera' element={<CustomWebcam />} />
           <Route path='/your-pics' element={<YourPics />} />
           <Route path='/upload-pics' element={<UploadPics />} />
-          <Route path='share-with-others' element={<ShareWithOthers />} />
           <Route path='/modal' element={<Modal />} />
         </Routes>
       </div>
